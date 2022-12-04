923014c43c50622787cd082c6497fa69c1609685
InputBlackBoxOutput <rutuparn.pawar@outlook.com>
2022-12-04 11:34:52 -0600

    Script modified

--- app.py
+++ app.py
@@ -6,16 +6,48 @@ st.title("KiCad Footprint Resizer")
 
 footprint = st.file_uploader("Upload footprint file", type=['kicad_mod'])
 
+
+def scale_start(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(start {x} {y})'
+
+
+def scale_end(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(end {x} {y})'
+
+
+def scale_width(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(width {x} {y})'
+
+
+def scale_size(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(size {x} {y})'
+
+
+def scale_xy(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(xy {x} {y})'
+
+
+def scale_at(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(at {x} {y})'
+
+
 if footprint is not None:
     with open("footprint", 'wb') as footprint_file:
         footprint_file.write(footprint.read())
 
     scale = st.number_input(label="Scale", value=0.5)
-
-    def scalexy(val):
-        x = float(val.group(1)) * scale
-        y = float(val.group(2)) * scale
-        return '(xy {} {})'.format(x, y)
 
     try:
         # Source: https://gist.github.com/urish/9c5b4aea6362da086541be14acdf0f72#gistcomment-4298113
@@ -23,8 +55,14 @@ if footprint is not None:
 
         with open("footprint", 'r') as file:
             for line in file.read().splitlines():
-                output += re.sub(r'\(xy ([0-9-.]+) ([0-9-.]+)\)', scalexy, line)
-                output += '\n'
+                line = re.sub(r'\(start ([0-9-.]+) ([0-9-.]+)\)', scale_start, line)
+                line = re.sub(r'\(end ([0-9-.]+) ([0-9-.]+)\)', scale_end, line)
+                line = re.sub(r'\(width ([0-9-.]+) ([0-9-.]+)\)', scale_width, line)
+                line = re.sub(r'\(size ([0-9-.]+) ([0-9-.]+)\)', scale_size, line)
+                line = re.sub(r'\(xy ([0-9-.]+) ([0-9-.]+)\)', scale_xy, line)
+                line = re.sub(r'\(at ([0-9-.]+) ([0-9-.]+)\)', scale_at, line)
+
+                output += line + '\n'
 
     except Exception:
         st.error("Something went wrong!")
