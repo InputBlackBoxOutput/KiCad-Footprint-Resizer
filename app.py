bb98c9dc795a6bc389518959a9a225d8d177eac7
InputBlackBoxOutput <rutuparn.pawar@outlook.com>
2022-12-04 19:32:02 -0600

    Fix application

--- app.py
+++ app.py
@@ -1,11 +1,75 @@
 import streamlit as st
 import re
-from script import process_line
 
-st.set_page_config(page_title='KiCad Footprint Resizer', page_icon='🛠️', layout='centered')
+st.set_page_config(page_title='KiCad Footprint Resizer',
+                   page_icon='🛠️', layout='centered')
 st.title("KiCad Footprint Resizer")
 
 footprint = st.file_uploader("Upload footprint file", type=['kicad_mod'])
+
+
+def scale_start(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(start {x} {y})'
+
+
+def scale_mid(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(mid {x} {y})'
+
+
+def scale_center(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(center {x} {y})'
+
+
+def scale_end(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(end {x} {y})'
+
+
+def scale_width(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(width {x} {y})'
+
+
+def scale_size(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(size {x} {y})'
+
+
+def scale_xy(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(xy {x} {y})'
+
+
+def scale_at(val):
+    x = float(val.group(1)) * scale
+    y = float(val.group(2)) * scale
+    return f'(at {x} {y}'
+
+
+def scale_drill(val):
+    x = float(val.group(1)) * scale
+    return f'(drill {x})'
+
+
+def scale_width(val):
+    x = float(val.group(1)) * scale
+    return f'(width {x})'
+
+
+def scale_thickness(val):
+    x = float(val.group(1)) * scale
+    return f'(thickness {x})'
+
 
 if footprint is not None:
     with open("footprint", 'wb') as footprint_file:
@@ -14,12 +78,24 @@ if footprint is not None:
     scale = st.number_input(label="Scale", value=0.5)
 
     try:
-        # Source: https://gist.github.com/urish/9c5b4aea6362da086541be14acdf0f72#gistcomment-4298113
         output = ""
 
         with open("footprint", 'r') as file:
             for line in file.read().splitlines():
-                output += process_line(line) + '\n'
+                line = re.sub(r'\(start ([0-9-.]+) ([0-9-.]+)\)', scale_start, line)
+                line = re.sub(r'\(mid ([0-9-.]+) ([0-9-.]+)\)', scale_mid, line)
+                line = re.sub(r'\(center ([0-9-.]+) ([0-9-.]+)\)', scale_center, line)
+                line = re.sub(r'\(end ([0-9-.]+) ([0-9-.]+)\)', scale_end, line)
+                line = re.sub(r'\(width ([0-9-.]+) ([0-9-.]+)\)', scale_width, line)
+                line = re.sub(r'\(size ([0-9-.]+) ([0-9-.]+)\)', scale_size, line)
+                line = re.sub(r'\(xy ([0-9-.]+) ([0-9-.]+)\)', scale_xy, line)
+                line = re.sub(r'\(at ([0-9-.]+) ([0-9-.]+)', scale_at, line)
+                line = re.sub(r'\(drill ([0-9-.]+)\)', scale_drill, line)
+                line = re.sub(r'\(width ([0-9-.]+)\)', scale_width, line)
+                line = re.sub(r'\(thickness ([0-9-.]+)\)', scale_thickness, line)
+
+                print(line)
+                output += line + '\n'
 
     except Exception:
         st.error("Something went wrong!")
@@ -27,12 +103,12 @@ if footprint is not None:
 
     if st.button("Resize"):
         st.markdown("-----")
-        st.success("Footprint resized successfully!") 
+        st.success("Footprint resized successfully!")
 
         st.download_button(
-            label="Download footprint", 
-            data=output, 
-            file_name="footprint.kicad_mod", 
+            label="Download footprint",
+            data=output,
+            file_name="footprint.kicad_mod",
             mime="plain/text")
 
 st.markdown("""
